43359e51e0f486815d380a812ecb6b2cf200ff79
zacha <zachary.campbell2006@outlook.com>
2025-04-09 16:51:48 +1000

    new online save

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+
 use std::ffi::OsStr;
 use std::path::PathBuf;
 use std::process::Command;
 use std::str::FromStr;
-use std::thread::sleep;
+use tokio::time::sleep;
 use std::time::{Duration, Instant, SystemTime, UNIX_EPOCH};
 use anyhow::Result;
 use serde::{Serialize, Deserialize};
+use serenity::all::{EventHandler};
 use sysinfo::{Process, System};
 
 #[derive(Clone, Serialize, Deserialize, Debug)]
@@ -15,17 +17,31 @@ struct Settings {
     pub save_location: String,
     pub backup_output_path: String,
     pub auto_save_interval_sec: u64,
+    pub discord_bot_token: String,
+    pub full_restart_timer_min: u64,
 }
 
-// const SAVE_INTERVAL: Duration = Duration::from_secs(60*3);
-
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     println!("Starting server tool");
 
-    let settings_json: Vec<u8> = std::fs::read("settings.json")?;
-    let settings: Settings = serde_json::from_slice(&settings_json)?;
+    let settings_json: Vec<u8> = std::fs::read("settings.json").unwrap();
+    let settings: Settings = serde_json::from_slice(&settings_json).unwrap();
     println!("{settings:#?}");
 
+
+    let bot = start_bot(&settings.discord_bot_token).await.unwrap();
+
+    // bot.send_msg("Test").await.unwrap();
+    // let file = include_bytes!("../example/out/1743999482077#FirstSuvivel.db");
+    // bot.send_file("Test.db", file).await.unwrap();
+
+    // testing
+    // loop {
+    //
+    // }
+
+    let kill_intervel = Duration::from_secs(settings.full_restart_timer_min * 60);
     let save_interval = Duration::from_secs(settings.auto_save_interval_sec);
     let mut sys = System::new_all();
 
@@ -33,25 +49,45 @@ fn main() -> Result<()> {
     if let Some(s) = scrap {
         println!("Killed already running processes");
         s.kill();
-        sleep(Duration::from_secs(5));
+        sleep(Duration::from_secs(5)).await;
     };
 
     println!("Starting event loop");
     let mut save_timer: Instant = Instant::now();
+    let mut kill_timer: Instant = Instant::now();
     loop {
         sys.refresh_all();
         if is_open(&sys).is_none() {
             println!("Not open");
-            open_game(&settings)?;
-            sleep(Duration::from_secs(30));
+            open_game(&settings).unwrap();
+            sleep(Duration::from_secs(30)).await;
         } else {
             // println!("Open");
             if save_timer.elapsed() > save_interval {
                 save_timer = Instant::now();
-                save_backup(&settings)?;
+                if let Err(e) = save_backup(&settings, &bot).await {
+                    println!("{e:?}");
+                }
             }
 
-            sleep(Duration::from_secs(10));
+            println!("{}", kill_timer.elapsed().as_secs());
+
+            if kill_timer.elapsed() > kill_intervel {
+                println!("Running periodic scrap machanic kill");
+                kill_timer = Instant::now();
+                if let Err(e) = save_backup(&settings, &bot).await {
+                    println!("{e:?}");
+                }
+
+                let scrap: Option<&Process> = is_open(&sys);
+                if let Some(s) = scrap {
+                    println!("Periodic scrap machanic kill");
+                    s.kill();
+                    sleep(Duration::from_secs(10)).await;
+                };
+            }
+
+            sleep(Duration::from_secs(10)).await;
         }
     }
 }
@@ -61,7 +97,7 @@ fn is_open(sys: &System) -> Option<&Process> {
     p
 }
 
-fn save_backup(settings: &Settings) -> Result<()> {
+async fn save_backup(settings: &Settings, bot: &Bot) -> Result<()> {
     println!("Saving backup");
 
     let path = PathBuf::from_str(&settings.save_location)?;
@@ -76,7 +112,9 @@ fn save_backup(settings: &Settings) -> Result<()> {
     let end_path = PathBuf::from_str(&settings.backup_output_path)?;
     let end_path = end_path.join(&n_name);
 
-    std::fs::write(&end_path, &file)?;
+    let _ = std::fs::write(&end_path, &file);
+
+    bot.send_file(&n_name, &file).await?;
 
     Ok(())
 }
@@ -89,7 +127,122 @@ fn open_game(settings: &Settings) -> Result<()> {
         .arg(&settings.steam_game_id)
         .arg("-open")
         .arg(&settings.save_location)
-        .spawn()?;
+        .spawn().unwrap();
 
     Ok(())
+}
+
+
+pub use bot::*;
+mod bot {
+    use std::sync::Arc;
+    use std::sync::mpsc::{channel, Sender};
+    use serenity::all::{ChannelId, Context, EventHandler, GatewayIntents, GuildId, Http, Ready};
+    use serenity::{async_trait, Client};
+    use tokio::spawn;
+    use anyhow::Result;
+    use serenity::builder::{CreateAttachment, CreateMessage};
+
+    const MAX_ATTACH_BYTES: usize = 5_000_000;
+    pub struct Bot {
+        pub http: Arc<Http>,
+        pub server: GuildId,
+        pub channel: ChannelId,
+    }
+    impl Bot {
+        pub async fn send_msg(&self, msg: &str) -> Result<()> {
+            let msg = CreateMessage::new().content(msg);
+
+            self.channel.send_message(&self.http, msg).await?;
+
+            Ok(())
+        }
+
+        pub async fn send_file(&self, name: &str, data: &[u8]) -> Result<()> {
+            let init_msg = CreateMessage::new().content(&format!("New file upload [{name}]"));
+            self.channel.send_message(&self.http, init_msg).await?;
+
+            for (i, chunk) in data.chunks(MAX_ATTACH_BYTES).into_iter().enumerate() {
+                println!("Sending attachment chunk {i}");
+                let attach = CreateAttachment::bytes(chunk, format!("Chunk{i}.chunk"));
+                let msg = CreateMessage::new().add_file(attach);
+                self.channel.send_message(&self.http, msg).await?;
+            };
+
+            Ok(())
+        }
+    }
+
+    struct BotHandler {
+        sender: Sender<(Arc<Http>, GuildId, ChannelId)>,
+    }
+    #[async_trait]
+    impl EventHandler for BotHandler {
+        async fn ready(&self, ctx: Context, data_about_bot: Ready) {
+            println!("Starting bot event handler");
+            let guilds = data_about_bot.guilds;
+            // for gid in guilds {
+            //             //     // let name = ctx.http.get_guild(gid.id).await.unwrap().name;
+            //             //     // println!("{name:?}")
+            //             // }
+
+            let server = guilds.first().expect("Add the bot to only one server with admin").id;
+            let channel = find_general_channel(&ctx, server).await.expect("No general channel");
+
+
+
+            self.sender.send((ctx.http.clone(), server, channel)).unwrap()
+        }
+    }
+
+    pub async fn find_general_channel(
+        ctx: &Context,
+        guild_id: GuildId,
+    ) -> Option<ChannelId> {
+        match ctx.http.get_channels(guild_id).await {
+            Ok(channels) => {
+                for channel in channels {
+                    if channel.name == "general" {
+                        return Some(channel.id);
+                    }
+                }
+                None
+            }
+            Err(e) => {
+                eprintln!("Error fetching channels for guild {}: {:?}", guild_id, e);
+                None
+            }
+        }
+    }
+
+
+    pub async fn start_bot(token: &str) -> Result<Bot> {
+        let intents = GatewayIntents::GUILD_MESSAGES
+            | GatewayIntents::GUILDS; // Add the GUILDS intent
+
+        let (s, r) = channel();
+
+
+        let mut client = Client::builder(token, intents)
+            .event_handler(BotHandler {
+                sender: s,
+            })
+            .await?;
+
+        spawn(async move {
+            client.start().await.expect("Bot killed itself");
+        });
+
+
+        let (http, server, channel) = r.recv()?;
+
+        let bot = Bot {
+            http,
+            server,
+            channel,
+        };
+
+        Ok(bot)
+    }
+
 }
